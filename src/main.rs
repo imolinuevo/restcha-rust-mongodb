a6ade4f05a7fd28b1679243c7ade53f54d42dd09
imolinuevo <imolinuevo@deloitte.es>
2018-11-02 12:18:25 +0100

    add rocket crate and hello endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+use rocket::request::Form;
+
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![hello]).launch();
+}
+
+#[derive(FromForm)]
+struct User {
+    name: String,
+    birthdate: String,
+}
+
+#[post("/hello", data = "<user>")]
+fn hello(user: Form<User>) -> String {
+    format!("username: {}, birthdate {}", user.name, user.birthdate)
 }
