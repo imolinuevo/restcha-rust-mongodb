115efc468eeae9d130e2cb5e051523079171ece8
imolinuevo <i.m.parron@opendeusto.es>
2018-11-09 12:19:08 +0100

    refactor request to handle JSON

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
-use rocket::request::Form;
+#[macro_use] extern crate serde_derive;
+extern crate rocket_contrib;
+use rocket_contrib::json::Json;
 
 
 fn main() {
     rocket::ignite().mount("/", routes![hello]).launch();
 }
 
-#[derive(FromForm)]
+#[derive(Serialize, Deserialize)]
 struct User {
     name: String,
     birthdate: String,
 }
 
-#[post("/hello", data = "<user>")]
-fn hello(user: Form<User>) -> String {
+#[post("/hello", format = "json", data = "<user>")]
+fn hello(user: Json<User>) -> String {
     format!("username: {}, birthdate {}", user.name, user.birthdate)
 }
