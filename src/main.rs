ffcfd1f037381f81d43a2c66c36b8b5c430fa6d4
imolinuevo <imolinuevo@deloitte.es>
2018-11-13 12:59:11 +0100

    add initial unit tests for rocket

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,18 @@
 #[macro_use] extern crate validator_derive;
 extern crate validator;
 
+use rocket::Rocket;
 use rocket_contrib::json::{Json, JsonValue};
 use validator::Validate;
 
 fn main() {
+    rocket().launch();
+}
+
+pub fn rocket() -> Rocket {
     rocket::ignite()
-    .register(catchers![not_found, invalid_entity])
+    .register(catchers![not_found, invalid_entity, bad_request])
     .mount("/", routes![hello])
-    .launch();
 }
 
 #[derive(Deserialize, Validate)]
@@ -36,12 +40,17 @@ fn hello(user: Json<User>) -> JsonValue {
             })
         ),
         Err(_e) => (
-            json!({
-                "message": "Invalid input data format",
-                "data": {}
-            })
+            bad_request()
         )
     }
+}
+
+#[catch(400)]
+fn bad_request() -> JsonValue {
+    json!({
+        "message": "Error 400: Bad request",
+        "data": {}
+    })
 }
 
 #[catch(404)]
@@ -55,7 +64,54 @@ fn not_found() -> JsonValue {
 #[catch(422)]
 fn invalid_entity() -> JsonValue {
     json!({
-        "message": "Error 422: Unprocessable Entity",
+        "message": "Error 422: Unprocessable entity",
         "data": {}
     })
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::Client;
+    use rocket::http::ContentType;
+    use rocket::http::Status;
+
+    #[test]
+    fn test_200() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.post("/hello")
+            .body("{ \"name\": \"alice\", \"birthdate\": \"10/10/1991\"}")
+            .header(ContentType::JSON)
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+
+    #[test]
+    fn test_400() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.post("/hello")
+            .body("{ \"name\": \"\", \"birthdate\": \"10/10/1991\"}")
+            .header(ContentType::JSON)
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        // should be Status::BadRequest
+    }
+
+    #[test]
+    fn test_404() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.get("/helloa").dispatch();
+        assert_eq!(response.status(), Status::NotFound);
+    }
+
+    #[test]
+    fn test_422() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.post("/hello")
+            .body("{}")
+            .header(ContentType::JSON)
+            .dispatch();
+        assert_eq!(response.status(), Status::UnprocessableEntity);
+    }
+    
 }
