3c7aa17377e13186451ef426b615d0d95fa559d6
imolinuevo <imolinuevo@deloitte.es>
2018-11-19 10:03:03 +0100

    split tests to module

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use rocket::Rocket;
 use rocket_contrib::json::{Json, JsonValue};
 use validator::Validate;
 
+#[cfg(test)] mod tests;
+
 fn main() {
     rocket().launch();
 }
@@ -67,51 +69,4 @@ fn invalid_entity() -> JsonValue {
         "message": "Error 422: Unprocessable entity",
         "data": {}
     })
-}
-
-#[cfg(test)]
-mod test {
-    use super::rocket;
-    use rocket::local::Client;
-    use rocket::http::ContentType;
-    use rocket::http::Status;
-
-    #[test]
-    fn test_200() {
-        let client = Client::new(rocket()).expect("valid rocket instance");
-        let response = client.post("/hello")
-            .body("{ \"name\": \"alice\", \"birthdate\": \"10/10/1991\"}")
-            .header(ContentType::JSON)
-            .dispatch();
-        assert_eq!(response.status(), Status::Ok);
-    }
-
-    #[test]
-    fn test_400() {
-        let client = Client::new(rocket()).expect("valid rocket instance");
-        let response = client.post("/hello")
-            .body("{ \"name\": \"\", \"birthdate\": \"10/10/1991\"}")
-            .header(ContentType::JSON)
-            .dispatch();
-        assert_eq!(response.status(), Status::Ok);
-        // should be Status::BadRequest
-    }
-
-    #[test]
-    fn test_404() {
-        let client = Client::new(rocket()).expect("valid rocket instance");
-        let response = client.get("/helloa").dispatch();
-        assert_eq!(response.status(), Status::NotFound);
-    }
-
-    #[test]
-    fn test_422() {
-        let client = Client::new(rocket()).expect("valid rocket instance");
-        let response = client.post("/hello")
-            .body("{}")
-            .header(ContentType::JSON)
-            .dispatch();
-        assert_eq!(response.status(), Status::UnprocessableEntity);
-    }
-    
 }
